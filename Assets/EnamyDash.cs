82ec32258bb5c14ef411242f22533b5475bda7a3
Decimalform <selmaonair@gmail.com>
2025-01-11 15:43:11 +0100

    Update EnamyDash.cs

--- Assets/EnamyDash.cs
+++ Assets/EnamyDash.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class EnamyDash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float enemyHealth;
+    public float dashDuration = 0.5f; // Duration of the dash
+    private Vector2 targetPosition;
 
     public void OnTriggerEnter2D(Collider2D col)
     {
-        PlayerAttack playerAttackComponent = col.GetComponent<PlayerAttack>();
-        if (playerAttackComponent != null)
+        Vector2 playerPosition = col.transform.position; // Get player position
+        StartCoroutine(DashTowardsPlayer(playerPosition));
+    }
+
+    private IEnumerator DashTowardsPlayer(Vector2 playerPosition)
+    {
+        Vector2 startPosition = transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < dashDuration)
         {
-            Debug.Log("Found");
+            transform.position = Vector2.Lerp(startPosition, playerPosition, (elapsedTime / dashDuration));
+            elapsedTime += Time.deltaTime; // Increment elapsed time
+            yield return null; // Wait for the next frame
         }
+
+        // Ensure the final position is exactly the player's position
+        transform.position = playerPosition;
     }
 }
