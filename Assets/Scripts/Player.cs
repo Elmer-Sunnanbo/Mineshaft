383313caef3a0f1d4fb9eb8b1968b1583ab1b42a
UnavailableNameIsNotAvailable <154227444+UnavailableNameIsNotAvailable@users.noreply.github.com>
2025-01-13 15:52:10 +0100

    Player Death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D myRigidbody;
     [SerializeField] float speed;
+
+    public int playerHP;
+    float iFrameTimer;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        playerHP = 4;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         myRigidbody.velocity = GetMoveVector() * speed;
+        iFrameTimer -= Time.deltaTime;
+    }
+    void Update()
+    {
+        if(playerHP < 1)
+        {
+            Debug.Log("Player died now and here");
+        }
+    }
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "enemy")
+        {
+            if(iFrameTimer < 0)
+            {
+                playerHP -= 1;
+                iFrameTimer = 1;
+            }
+        }
     }
 
     Vector2 GetMoveVector()
