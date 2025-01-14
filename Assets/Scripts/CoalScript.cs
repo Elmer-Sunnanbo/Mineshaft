2ad676b86332a4d577752257bd8b22e6a92bb68c
Decimalform <selmaonair@gmail.com>
2025-01-14 11:38:44 +0100

    Made coal prefab

--- Assets/Scripts/CoalScript.cs
+++ Assets/Scripts/CoalScript.cs
@@ -6,23 +6,6 @@ public class CoalScript : MonoBehaviour, IHittable
 {
     public CoalHolder coalHolder;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Hit();
-        }
-
-    }
-
     public void Hit()
     {
         // When coal is hit the gameObject is destroyed and the player gains coal
