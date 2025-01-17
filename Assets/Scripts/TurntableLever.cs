706e33d1967afba2cc7b0dd75513554987db6898
Gabriele-07-idk <154230107+Gabriele-07-idk@users.noreply.github.com>
2025-01-17 11:10:36 +0100

    Implementerade saker
    Sprites för blooden, stalagmiter, och animationerna för spaken

--- Assets/Scripts/TurntableLever.cs
+++ Assets/Scripts/TurntableLever.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class TurntableLever : MonoBehaviour, IHittable
 {
     [SerializeField] List<RailTile> connectedTables = new List<RailTile>();
+    [SerializeField] Animator LeverAni;
+    bool Lever1 = true;
+    bool Lever2 = false;
     public void Hit()
     {
         foreach (RailTile table in connectedTables)
         {
             table.RotateTurntable();
+        }
+        if(Lever2)
+        {
+            LeverAni.SetBool("LeverPressed2", false);
+            LeverAni.SetBool("LeverPressed1", true);
+            Lever2 = false;
+            Lever1 = true;
+        }
+        else if (Lever1)
+        {
+            LeverAni.SetBool("LeverPressed1", false);
+            LeverAni.SetBool("LeverPressed2", true);
+            Lever1 = false;
+            Lever2 = true;
         }
     }
 }
